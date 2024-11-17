f0c8c89e831b740078a580b3b7a11ffade3b03aa
KirillRV <kirillreges639@gmail.com>
2024-11-17 19:22:32 +0300

    HW 12 bug fixes

--- com.teachmeskills.hw.lesson_12/src/com/teachmeskills/hw/lesson_12/Constants/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.teachmeskills.hw.lesson_12.Constants;
-
-public interface Constants {
-    String Hex_Color_Pattern = "^#([0-9a-fA-F]{6})$";
-
-    String URL_PATTERN = "^(http|https)://.+$";
-}
